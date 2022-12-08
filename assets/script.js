530be7db27e10efd18d8ffa86ccba2fce043fc94
kassrojas <kassandrarojass@gmail.com>
2022-12-08 09:44:46 -0500

    rendering local WORKS yaygit add -A had to define my variables better

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,38 @@
-
 renderLocal();
 
-
 $(function timeColorChange (){
-
-  var currentHour = dayjs().hour();
-
+  
   $('.description').each(function () {
-
     $(this).parent().removeClass('past present future');
-
-    var timeBlock = parseInt($(this).parent().attr('id').replace('hour-', ''));
-
+    
+    // gives us just the number in id
+    var timeBlock = parseInt($(this).parent().attr('id').replace('hour-', '')); 
+    var currentHour = dayjs().hour();
+    
     if (timeBlock < currentHour){
       $(this).addClass('past');
     } else if (timeBlock === currentHour) {
       $(this).addClass('present');
     } else {
       $(this).addClass('future');
-    }
-
+    } 
   });
-   
+  
+  //upon clicking .saveBtn, val() passed into localStorage
   $('.saveBtn').click(function (){
     var textArea = $(this).siblings().eq(1).val();
     var time = $(this).parent().attr('id').split('-')[1];
-    localStorage.setItem(textArea, time);
+    localStorage.setItem('hour-' + time, textArea);
   })
+  
 
-
+  
 });
 
 function renderLocal(){
-   for (var i = 9; i <= 17; i++){
-    $("#hour-" + i).children('description').val(localStorage.getItem('hour-' + i));
-   }
-}
+  console.log('rendering');
+  for (var i = 9; i <= 17; i++){
+    $("#hour-" + i).children('.description').val(localStorage.getItem('hour-' + i));
+  }
+
+};
